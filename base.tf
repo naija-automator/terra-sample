adb78b589b380a5d876aeb11f500745b046601b4
naija-automator <phunkyjohnkey@gmail.com>
2019-06-01 16:22:08 +0100

    Launch Configuration added plus data source for AZs

--- base.tf
+++ base.tf
@@ -1,47 +1,59 @@
+data "aws_availability_zones" "all" {}
+
 provider "aws" {
   region = "eu-west-2"
 }
 
-resource "launch_configuration"" "terra1" {
-  ami           = "ami-07dc734dc14746eab"
-  instance_type = "t2.micro"
-  security_group_ids = ["${aws_security_group.terra1-sg.id}"]
+resource "aws_launch_configuration" "terra1" {
+  image_id           = "ami-07dc734dc14746eab"
+  instance_type      = "t2.micro"
+  security_groups    = ["${aws_security_group.terra1-sg.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World! This is Terra1." > index.html
               nohup busybox httpd -f -p "{$var.server_port}" &
               EOF
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
-
 resource "aws_security_group" "terra1-sg" {
-  name   = "terra1-sec-grp"
+  name = "terra1-sec-grp"
 
   ingress {
-    from_port  =  "${var.server_port}"
-    to_port    =  "${var.server_port}"
-    protocol   =  "tcp"
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags {
     Name = "terra1-billing"
   }
-  
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
-variable "server_port" { 
+variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  default = 8080
+  default     = 8080
 }
 
-output "public_ip" {
-  value = "${aws_instance.terra1.public_ip}"
+resource "aws_autoscaling_group" "terra1-asg" {
+  launch_configuration = "${aws_launch_configuration.terra1.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 6
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-terra1"
+    propagate_at_launch = true
+  }
 }
