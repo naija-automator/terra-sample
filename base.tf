45cfd8c5ffa31370427bfcb3f8f8a2d44ee10001
naija-automator <phunkyjohnkey@gmail.com>
2019-06-01 14:58:53 +0100

    base terraform config file edited

--- base.tf
+++ base.tf
@@ -2,15 +2,37 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "base" {
-  ami           = "ami-0009a33f033d8b7b6"
+resource "aws_instance" "terra1" {
+  ami           = "ami-07dc734dc14746eab"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.terra1-sg.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World! This is Terra1." > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags {
-    Name = "phunky-example"
+    Name = "terra_vms"
   }
 }
 
-resource "aws_eip" "base" {
-  instance = "${aws_instance.base.id}"
+resource "aws_eip" "terra1_eip" {
+  instance = "${aws_instance.terra1.id}"
+}
+
+resource "aws_security_group" "terra1-sg" {
+  name   = "terra1-sec-grp"
+
+  ingress {
+    from_port  =  8080
+    to_port    =  8080
+    protocol   =  "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "terra1-billing"
+  }
 }
