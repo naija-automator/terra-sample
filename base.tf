edd4d85c431abdccfb1e23f98f390b5661e6085e
naija-automator <phunkyjohnkey@gmail.com>
2019-06-01 19:44:49 +0000

    Additional components added to deploy web cluster

--- base.tf
+++ base.tf
@@ -48,6 +48,9 @@ resource "aws_autoscaling_group" "terra1-asg" {
   launch_configuration = "${aws_launch_configuration.terra1.id}"
   availability_zones   = ["${data.aws_availability_zones.all.names}"]
 
+  load_balancers       = ["${aws_elb.terra1_elb.name}"]
+  health_check_type    = "ELB"
+
   min_size = 2
   max_size = 6
 
@@ -56,4 +59,47 @@ resource "aws_autoscaling_group" "terra1-asg" {
     value               = "terraform-asg-terra1"
     propagate_at_launch = true
   }
+}
+
+resource "aws_elb" "terra1_elb" {
+  name         =   "terra1elb"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb-sg.id}"]
+
+  listener {
+    lb_port           =   80
+    lb_protocol       =   "http"
+    instance_port     =   "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold      =  2
+    unhealthy_threshold    =  2
+    timeout                =  3
+    interval               =  30
+    target                 =  "HTTP:${var.server_port}/"
+  }
+}
+
+resource "aws_security_group" "elb-sg" {
+  name         =   "terra1-elb-sg"
+
+  ingress {
+    from_port    =   80
+    to_port      =   80
+    protocol     =   "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port    =   0
+    to_port      =   0
+    protocol     =   "-1"
+    cidr_blocks  =   ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name" {
+  value  =  "${aws_elb.terra1_elb.dns_name}"
 }
