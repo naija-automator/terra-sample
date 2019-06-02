8dabbed65638509d1385e24de19df6829dadfa21
naija-automator <phunkyjohnkey@gmail.com>
2019-06-02 20:07:50 +0000

    Added S3 bucket & DynamoDB for remote state

--- main/main.tf
+++ main/main.tf
@@ -1,9 +1,14 @@
+#Creates S3 Bucket & configures as backend for State files
+
 provider "aws" {
   region  =  "eu-west-2"
 }
 
+#terraform state file setup
+#Create s3 bucket
+
 resource "aws_s3_bucket" "terraform_state_backup" {
-  bucket =  "terraform-bicky"
+  bucket =  "phunkytech-tf-state-s3"
 
   versioning {
     enabled  =  true
@@ -11,5 +16,37 @@ resource "aws_s3_bucket" "terraform_state_backup" {
 
   lifecycle {
     prevent_destroy = true 
+  }
+  
+  tags {
+    Name  =  "S3 Remote Terraform State Store"
+  }
+}
+
+#create the s3 backend resource
+
+terraform {
+  backend  "s3" {
+  encrypt = true
+  bucket  = "phunkytech-tf-state-s3"
+  dynamodb_table = "phunky-tf-lock-dynamo"
+  region  =  "eu-west-2"
+  key     =  "home/bruce/terraform/terra-sample/terraform.tfstate"
+  }
+}
+
+resource "aws_dynamodb_table" "phunky-dynamodb-tf-lock" {
+  name  =  "phunky-tf-lock-dynamo"
+  hash_key  =  "LockID"
+  read_capacity  =  20
+  write_capacity =  20
+
+  attribute  {
+    name  = "LockID"
+    type  = "S"
+  }
+
+  tags {
+    Name   =  "DynamoDB Terraform State Lock Table"
   }
 }
